dfbd013eb1ab603862befd9e419bd7089eda0516
CTL-ALT-DEL <fallbrookdave@gmail.com>
2026-07-05 12:37:33 -0600

    v19 Pro 2.0

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME="fault-locator-pro-v18";
+const CACHE_NAME="fault-locator-pro-v19";
 const FILES=["./","index.html","styles.css","app.js","manifest.json","icons/icon-180.png","icons/icon-192.png","icons/icon-512.png"];
 self.addEventListener("install",e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(FILES)))});
 self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))));self.clients.claim()});
